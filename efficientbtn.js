b6303c1fae611ef156d746c5bccf415035e37fdb
splerg <157251955+splerg@users.noreply.github.com>
2024-01-20 22:20:22 +0000

    Add files via upload

---
+++ efficientbtn.js
@@ -0,0 +1,93 @@
+//variables
+let clicks = 0;
+let sitetitle = document.getElementById("sitetitle");
+let clickscounter = document.getElementById("clickcounter");
+let mainbtn = document.getElementById("mainclicker");
+let cpscounter = document.getElementById("cps");
+let abbreviation = ["", "m", "b", "t", "qa", "qi", "sx", "sp", "oc", "no", "dc"];
+
+//Autoclickers
+let Autoclicker = document.getElementById("Autoclicker");
+let Megaclicker = document.getElementById("Megaclicker");
+let Superclicker = document.getElementById("Superclicker");
+
+//Upgrades
+let clickupg = document.getElementById("basicupgrade");
+let clickupgprice = 5000;
+let clickupgpower = 1;
+let basiclabel = document.getElementById("basiclabel");
+
+//upgrade variables
+let autoclickerowned = 0;
+let autoprice = 10;
+let autopower = 1;
+let autolabel = document.getElementById("autolabel")
+let megaautoowned = 0;
+let megaprice = 500;
+let megapower = 5;
+let megalabel = document.getElementById("megalabel")
+let superautoowned = 0;
+let superprice = 5000;
+let superpower = 100;
+let superlabel = document.getElementById("superlabel")
+
+let cps = 0;
+let clickpower = 1;
+
+//Formatting
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+//main button
+mainbtn.onclick = () => {
+    clicks += clickpower;
+    clickscounter.textContent = ("Clicks: " + numberWithCommas(clicks));
+}
+
+//Autoclicker Buy
+buyAutoClick = (auto, price, autoowned, clickadd, label) => {
+    auto.onclick = () => {
+        if (clicks >= price && clicks - price > 0) {
+            clicks -= price;
+            clickscounter.textContent = ("Clicks: " + numberWithCommas(clicks));
+            autoowned += 1;
+            cps += clickadd;
+            cpscounter.textContent = ("CPS: " + cps);
+            price = (price += Math.round(price/100*15));
+            label.textContent = (" - Costs: "+ price +" (Owned: " + autoowned + ")");
+        }
+    }
+}
+
+buyAutoClick(Autoclicker, autoprice, autoclickerowned, autopower, autolabel);
+buyAutoClick(Megaclicker, megaprice, megaautoowned, megapower, megalabel);
+buyAutoClick(Superclicker, superprice, superautoowned, superpower, superlabel);
+
+//Click Upgrades
+buyClickUpgrade = (upg, upgprice, upgpower, upglabel) => {
+    upg.onclick = () => {
+        if (clicks >= upgprice && clicks - upgprice > 0) {
+            clicks -= upgprice;
+            clickscounter.textContent = ("Clicks: " + numberWithCommas(clicks));
+            clickpower += upgpower;
+            clickupg.disabled = true;
+            upglabel.textContent = ("Owned!");
+        }
+    }
+}
+
+buyClickUpgrade(clickupg, clickupgprice, clickupgpower, basiclabel);
+
+//Autoclicking
+cps_click = () => {
+    clicks += cps
+    clickscounter.textContent = ("Clicks: " + numberWithCommas(clicks))
+    sitetitle.textContent = ((clicks) + " Clicks - Clicker Game")
+}
+
+setInterval(cps_click, 1000)
